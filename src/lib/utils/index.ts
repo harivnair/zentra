a93ee4333e7845cc5879f5b4a6081a91a8a111a2
Amith Alexander <amithalexander26@gmail.com>
2026-04-14 08:07:51 +0000

    --amend

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,8 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-import { AVATAR_COLORS } from "@/constants";
-
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
